c739488f4ea6feba607e70305d9c624852df19fc
AyushiSingh14399 <53054160+AyushiSingh14399@users.noreply.github.com>
2020-06-12 13:12:34 +0530

    first commit

---
+++ backend/data.js
@@ -0,0 +1,47 @@
+export default {
+    products: [
+      {
+        _id: '1',
+        name: 'Laptop1',
+        category: 'Laptop',
+        image: '/images/d1.jpg',
+        price: 60000,
+        brand: 'Hp',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 5,
+      },
+      {
+        _id: '2',
+        name: 'Laptop2',
+        category: 'Laptop',
+        image: '/images/d1.jpg',
+        price: 50000,
+        brand: ' Hp',
+        rating: 4.0,
+        numReviews: 5,
+        countInStock: 3,
+      },
+      {
+        _id: '3',
+        name: 'Mouse1',
+        category: 'Mouse',
+        image: '/images/d2.jpg',
+        price: 700,
+        brand: ' sony',
+        rating: 4.5,
+        numReviews: 8,
+        countInStock: 8,
+      }, {
+        _id: '4',
+        name: 'Mouse2',
+        category: 'Mouse',
+        image: '/images/d2.jpg',
+        price: 600,
+        brand: ' sony',
+        rating: 4.5,
+        numReviews: 9,
+        countInStock: 0,
+      },
+    ]
+  }
